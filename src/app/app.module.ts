e607bd20ce6114831b801a3a6e97e1cbf052234c
Jenn Le <jenn.thakugan@icloud.com>
2017-06-15 13:30:42 -0500

    Header completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing/app-routing.module'
+
 import { AppComponent } from './app.component';
+import { JennNavComponent } from './navigation/jenn-nav/jenn-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JennNavComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
+
 export class AppModule { }
---
+++ src/app/navigation/jenn-nav/jenn-nav.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jenn-nav',
+  templateUrl: './jenn-nav.component.html',
+  styleUrls: ['./jenn-nav.component.css']
+})
+
+export class JennNavComponent implements OnInit {
+
+  title = "JENN LE";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
